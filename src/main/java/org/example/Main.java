56ad8cd6640c04b1d8a6d00e9cbe8132a6f67266
Ricardo Delgado <Ricardo.Delgado@endava.com>
2023-04-28 18:24:25 -0500

    Intro

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        float value = 102.0f; // DOES NOT COMPILE
+    }
+}
